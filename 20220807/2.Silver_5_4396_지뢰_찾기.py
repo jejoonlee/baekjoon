22ede08c76cd9522421d43bfa6d3fb63344c09d4
jejoonlee <jejoonlee1996@gmail.com>
2022-08-08 21:56:58 +0900

    add 20220808, update 20220807

--- 20220807/2.Silver_5_4396_지뢰_찾기.py
+++ 20220807/2.Silver_5_4396_지뢰_찾기.py
@@ -18,32 +18,35 @@ mine_field = [list(input()) for _ in range(N)]
 sel = [list(input()) for _ in range(N)]
 ans = [['.'] * N for _ in range(N)]
 
-dx = [-1, 0, 1, -1, 1, -1, 0, 1] # column
-dy = [-1, -1, -1, 0, 0, 1, 1, 1] # row
+dx = [-1, -1, -1, 0, 0, 1, 1, 1] 
+dy = [-1, 0, 1, -1, 1, -1, 0, 1] 
 
 
-# 순회는 아무 방향
-for y in range(N):
-    for x in range(N):
-        bomb = 0     
-        for a in range(8):
-            sx = dx[a] + y
-            sy = dy[a] + x
+# 이차원 리스트 순회
+for x in range(N):
+    for y in range(N):
+        
+        bomb = 0
+        # 순회하는데 x가 있을 때에
+        if mine_field[x][y] == '.' and sel[x][y] == 'x':
+        
+            #델타 탐색 8면
+            for i in range(8):
+                sx = dx[i] + x
+                sy = dy [i] + y
 
-            if sel[y][x] == 'x':
-            # 리스트에서 벗어나면 안됨
+                # 델타 탐색이 범위 내에 있고
+                # 주변에 지뢰가 있으면 bomb에 1을 더하기
                 if 0 <= sx < N and 0 <= sy < N:
                     if mine_field[sx][sy] == '*':
                         bomb += 1
-                    # ans 변수에 지뢰 개수 저장하기
-                    ans[y][x] = bomb
+            ans[x][y] = bomb
 
-        if mine_field[y][x] == '*':
-        # sel 리스트에서 x인데, mine_field에서 지뢰면
+        if mine_field[x][y] == '*' and sel[x][y] == 'x':
             for a in range(N):
                 for b in range(N):
-                    if mine_field[y][x] == '*':
-                        ans[y][x] = '*'
+                    if mine_field[a][b] == '*':
+                        ans[a][b] = '*'
 
 for i in range(N):
     for j in range(N):
